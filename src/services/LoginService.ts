d0d141e184c53906c61455c43053f9342f605c06
Ariane <arianelecias03@gmail.com>
2024-01-04 05:39:00 +0800

    fix unused data

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -3,6 +3,7 @@ import { auth, AuthErrorMessages } from "../firebase";
 
 export const login = (email: string, password: string, callback: (success: Boolean, m: string) => any) => {
     signInWithEmailAndPassword(auth, email, password).then(data => {
+        console.log(data);
         callback(true, "Success");
     }).catch(err => {
         console.log(err.code);
